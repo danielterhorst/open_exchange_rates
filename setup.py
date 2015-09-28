ee05f4a2af81ec3d8a32e33acf3ae6a0b8bcd3cc
danielterhorst <mail@danielterhorst.nl>
2015-09-28 10:34:32 +0200

    * Used  to convert GitHub's Markdown to reStructured Text * Added setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import os
+
+from distutils.core import setup
 from setuptools import setup
+import pypandoc
 
 setup(
     name='open_exchange_rates',
-    version='1.0a1',
+    version='1.0a2',
     description='An Open Exchange Rates python client',
+    long_description=pypandoc.convert('README.md', 'rst'),
     url='https://github.com/danielterhorst/open_exchange_rates',
     author='Daniel ter Horst',
     author_email='mail@danielterhorst.nl',
